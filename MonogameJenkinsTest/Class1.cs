523803e2645a9e6139ccb430bb6116d7de4530ce
RasmusThorsen <rallethor@hotmail.com>
2018-05-01 13:44:49 +0200

    tilføjet projektet

---
+++ MonogameJenkinsTest/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace MonogameJenkinsTest
+{
+    public class Class1 : Game
+    {
+        protected override void Update(GameTime gt)
+        {
+            //comment
+        }
+    }
+
+}
